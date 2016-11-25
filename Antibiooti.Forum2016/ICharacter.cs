3b0c3d404b617a989ff299852d2ba10e1b036ccc
Guido <g.loupias@tue.nl>
2016-11-25 10:07:08 +0100

    character signature

---
+++ Antibiooti.Forum2016/ICharacter.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Antibiooti.Forum2016
+{
+    interface ICharacter 
+    {
+        
+        void printCharacter(char ch, int pos, ConsoleColor color = ConsoleColor.White);
+    }
+}
