dff9d5afceaab858351ad5fe48f1b11baf9b0b3c
guidogithub <g.loupias@tue.nl>
2016-11-25 10:54:42 +0100

    Character

--- Antibiooti.Forum2016/Character.cs
+++ Antibiooti.Forum2016/Character.cs
@@ -5,8 +5,20 @@ namespace Antibiooti.Forum2016
 
     class Character : ICharacter
     {
-        private char[,] _buffer = new char[5,5];
-        public char[,] buffer() { return _buffer; }
+        private char[,] _buffer;
+
+        Character(char c) {
+            this._buffer = new char[5,5];
+            for(int i = 0; i < 5; ++i)
+                for(int j = 0; j < 5; ++j)
+                    this._buffer[i,j] = ' ';
+
+            if (c == '.') {
+                _buffer[2,2] = '*';
+            } 
+        }
+
+        public char[,] buffer() { return this._buffer; }
     }
 
 }
