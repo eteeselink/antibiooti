86aac54c3115434784bfd1b3d9d0d7dc6ae434ea
Dmitriy Kondrashov <d.kondrashov@tue.nl>
2016-11-25 12:34:56 +0100

    lalala

--- Antibiooti.Forum2016/Text.cs
+++ Antibiooti.Forum2016/Text.cs
@@ -3,30 +3,37 @@ using System;
 namespace Antibiooti.Forum2016
 {
 public class Text {
-string text, color;
-char[] arrayText, arrayColor;
+string text;
+string[] color;
+char[] arrayText;
     public void processText(string s) {
         string[] source;
         string text, color;
         source = s.Split(';');
         this.text = source[0];//text
-        this.color = source[1];//color
+        this.color = source[1].Split(',');//color
+        ConsoleColor[] consoleColors = new ConsoleColor[this.color.Length];
+
+        for (int i = 0; i < this.color.Length; i++) {
+            if (this.color[i] == "b") {
+                consoleColors[i] = ConsoleColor.Blue;
+            }
+            else if (this.color[i] == "r") {
+                consoleColors[i] = ConsoleColor.Red;
+            }
+            else if (this.color[i] == "g") {
+                consoleColors[i] = ConsoleColor.Green;
+            }
+
+        }
+
         this.arrayText = source[0].ToCharArray();
-        this.arrayColor = source[1].ToCharArray();
         Character[] arrayLight = new Character[this.arrayText.Length];
         Screen screen = new Screen();
         for(int i = 0; i < this.arrayText.Length; i ++) {
             arrayLight[i] = new Character(this.arrayText[i]);
-            screen.printCharacter(arrayLight[i], i, ConsoleColor.White);
+            screen.printCharacter(arrayLight[i], i, consoleColors[i]);
         }
     }
-    public string getText() {
-        return this.text;
-    }
-    public string getColor() {
-        return this.color;
-    }
-    
-
 } 
 }
