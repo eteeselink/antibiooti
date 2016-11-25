1e6dd087ee15abaf906be1603528f123d3f97fb7
Dmitriy Kondrashov <d.kondrashov@tue.nl>
2016-11-25 10:15:26 +0100

    made a fix

--- Antibiooti.Forum2016/Text.cs
+++ Antibiooti.Forum2016/Text.cs
@@ -1,17 +1,27 @@
 public class Text {
 string text, color;
+char[] arrayText, arrayColor;
     public void processText(string s) {
         string[] source;
         string text, color;
         source = s.Split(';');
-        this.text = source[0];
-        this.color = source[1];
+        this.text = source[0];//text
+        this.color = source[1];//color
+        this.arrayText = source[0].value.ToCharArray();
+        this.arrayColor = source[1].value.ToCharArray();
+        //loop through array text
+        //inside the loop 
+        //send the posi (loop i),char of array text, char of array color 
+        //ddne
+        for(int i = 0; i < this.arrayText.length; i ++) {
+            //
+        }
     }
 
-    public void getText() {
+    public string getText() {
         return this.text;
     }
-    public void getColor() {
+    public string getColor() {
         return this.color;
     }
     
